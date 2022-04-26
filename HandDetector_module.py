51787675bbd03602c20ae1d996bd0ed485b0dc7b
Srijon Basak <37615010+srijonbasak@users.noreply.github.com>
2022-04-27 02:23:35 +0600

    Update HandDetector_module.py

--- HandDetector_module.py
+++ HandDetector_module.py
@@ -9,49 +9,52 @@ class handDetect():
     self.min_detection_co = min_detection_co
     self.min_tracing_co = min_tracing_co
     self.modelComplexity = modelComplexity
-
     self.np_hands = mp.solutions.hands
     self.mpDraw = mp.solutions.drawing_utils
     self.hands = self.np_hands.Hands(self.mode, self.max_hands, self.modelComplexity, self.min_detection_co, self.min_tracing_co)
-    
-
 
   def findhands(self, img, draw=True):
     #convert bgr to rgb
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+    
     #prosses hand in img
     self.result = self.hands.process(rgb_img)
-
+    
+    #drawing
     if self.result.multi_hand_landmarks:
       for handLm in self.result.multi_hand_landmarks:
         if draw:
           self.mpDraw.draw_landmarks(img, handLm, self.np_hands.HAND_CONNECTIONS)
     return img
     
-  def findposition(self, img, handno = 0 , draw = False):
+  def findposition(self, img, handno = 0):
     landmarklist = []
     if self.result.multi_hand_landmarks:
       myhand = self.result.multi_hand_landmarks[handno]
       for id, lm in enumerate(myhand.landmark):
         h, w, c = img.shape
-        cx, cy = int(lm.x*w), int(lm.y*h)
+        cx, cy = int(lm.x*w), int(lm.y*h)#geting possition in screen
         landmarklist.append([id,cx,cy])
     return landmarklist
 
 def main():
   cap = cv2.VideoCapture(0)#webcam
+  
   #for fps count
   Currentime = 0
   previoustime = 0
+  
   detector = handDetect()
+  
   while True:
       #read image
       success, img = cap.read()
+      
       img = detector.findhands(img)
       lmlist = detector.findposition(img)
       if len(lmlist) != 0:
         print(lmlist)
+        
       #fps
       Currentime = time.time()
       fps = 1/(Currentime-previoustime)
